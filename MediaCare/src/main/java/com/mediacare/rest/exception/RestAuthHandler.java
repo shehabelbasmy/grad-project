8f805cb40066b652d8ab546255c51058098717c8
Shehab <shehabelbasmy123@gmail.com>
2022-06-04 22:28:08 +0200

    add status property for json response for faild login as esmail require

--- MediaCare/src/main/java/com/mediacare/rest/exception/RestAuthHandler.java
+++ MediaCare/src/main/java/com/mediacare/rest/exception/RestAuthHandler.java
@@ -1,15 +1,18 @@
 package com.mediacare.rest.exception;
 
+import java.io.IOException;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
+import com.google.gson.Gson;
 
 @Component
 public class RestAuthHandler implements AuthenticationEntryPoint {
@@ -18,8 +21,15 @@ public class RestAuthHandler implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException ex) throws IOException, ServletException {
         response.setStatus(HttpStatus.OK.value());
-        byte[] message = ex.getMessage().getBytes(StandardCharsets.UTF_8);
-        response.getOutputStream().write(message);
+        String json = new Gson().toJson(
+    		Map.ofEntries(
+				Map.entry("error", ex.getMessage()),
+    			Map.entry("status", "false")
+    		)
+		);
+        response.setContentType("application/json");
+        response.getWriter().write(json);
+        
     }
 
 }
