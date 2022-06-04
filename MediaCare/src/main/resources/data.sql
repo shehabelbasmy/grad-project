2fcd3c058e023bd3a836ef5b0fdba3c45b35fc2b
shehab <shehabelbasmy>
2022-06-04 19:30:09 +0200

    Refactoring

--- MediaCare/src/main/resources/data.sql
+++ MediaCare/src/main/resources/data.sql
@@ -6,7 +6,5 @@ VALUES (3,'ADMIN','mohamed@gmail.com','1','Ahmed','El Ashry','$2a$12$IQVQ.mNRBvX
 (2,'PATIENT','ahmed@gmail.com','1','Yasser','Mohamed','$2a$12$yWEJoJeo/wQnyJOCoige.ujOWbb2YJqAQwQRsiXP96yNvXv9CL7PC');
 -- patient password mohamed
 -- admin password ahmed
-INSERT INTO `admin`
-VALUES (2);
 INSERT INTO `patient`
 VALUES (null,'MALE',null,3)
